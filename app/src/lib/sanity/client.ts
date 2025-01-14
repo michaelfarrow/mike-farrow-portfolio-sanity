949ce74bafcb958584f10dc05f2934797ede06ce
Mike Farrow <mike@farrow.io>
2025-01-14 16:19:28 +0000

    fixing build issues

--- app/src/lib/sanity/client.ts
+++ app/src/lib/sanity/client.ts
@@ -1,37 +1,37 @@
 import { createClient } from '@sanity/client';
 import { config } from '@common/config';
 
-import {
-  AllSanitySchemaTypes,
-  internalGroqTypeReferenceTo,
-} from '@/types/sanity';
+// import {
+//   AllSanitySchemaTypes,
+//   internalGroqTypeReferenceTo,
+// } from '@/types/sanity';
 
-export type SchemaTypes = AllSanitySchemaTypes['_type'];
+// export type SchemaTypes = AllSanitySchemaTypes['_type'];
 
-export type SchemaType<T extends SchemaTypes> = UndefinedToNull<
-  ExpandRefs<Extract<AllSanitySchemaTypes, { _type: T }>>
->;
+// export type SchemaType<T extends SchemaTypes> = UndefinedToNull<
+//   ExpandRefs<Extract<AllSanitySchemaTypes, { _type: T }>>
+// >;
 
-export type UnionUndefinedToNull<T> = T extends undefined ? null : T;
+// export type UnionUndefinedToNull<T> = T extends undefined ? null : T;
 
-export type UndefinedToNull<T extends object> = {
-  [Prop in keyof T]: T[Prop] extends object
-    ? UndefinedToNull<T[Prop]>
-    : UnionUndefinedToNull<T[Prop]>;
-};
+// export type UndefinedToNull<T extends object> = {
+//   [Prop in keyof T]: T[Prop] extends object
+//     ? UndefinedToNull<T[Prop]>
+//     : UnionUndefinedToNull<T[Prop]>;
+// };
 
-export type ExpandRefs<T extends object> = {
-  [K in keyof T]: NonNullable<T[K]> extends {
-    _type: 'reference';
-    [internalGroqTypeReferenceTo]?: SchemaTypes;
-  }
-    ? SchemaType<
-        NonNullable<NonNullable<T[K]>[typeof internalGroqTypeReferenceTo]>
-      >
-    : NonNullable<T[K]> extends object
-      ? ExpandRefs<NonNullable<T[K]>>
-      : string;
-};
+// export type ExpandRefs<T extends object> = {
+//   [K in keyof T]: NonNullable<T[K]> extends {
+//     _type: 'reference';
+//     [internalGroqTypeReferenceTo]?: SchemaTypes;
+//   }
+//     ? SchemaType<
+//         NonNullable<NonNullable<T[K]>[typeof internalGroqTypeReferenceTo]>
+//       >
+//     : NonNullable<T[K]> extends object
+//       ? ExpandRefs<NonNullable<T[K]>>
+//       : string;
+// };
 
 // type Test2 = PartialOnUndefinedDeep<{
 //   one: string;
