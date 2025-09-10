4f6faa028e885de0d7644182865fb01daf5d2146
Mike Farrow <mike@farrow.io>
2025-09-10 19:31:35 +0100

    tweaking next dist

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: 'dist',
   images: {
     qualities: [25, 50, 75],
     deviceSizes: [160, 320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
