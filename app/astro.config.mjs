6faf667016d5a859de4a040ff5fc80f5a4c50c74
Mike Farrow <mike@farrow.io>
2025-10-06 15:11:46 +0100

    trying to add vercel adaptor

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import react from '@astrojs/react';
+import vercelServerless from '@astrojs/vercel/serverless';
 import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 
 import { config } from '../common/config';
 import { plugins as postCssPlugins } from './postcss.config.mjs';
 
+const visualEditingEnabled =
+  import.meta.env.SANITY_VISUAL_EDITING_ENABLED === 'true';
+
 export default defineConfig({
+  output: visualEditingEnabled ? 'server' : 'static',
+  adapter: visualEditingEnabled ? vercelServerless({}) : undefined,
   integrations: [
     sanity({
       ...config.studio,
