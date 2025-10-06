58584e4036893349a9a445f039efcd5390d72c8c
Mike Farrow <mike@farrow.io>
2025-10-06 15:21:31 +0100

    trying to fix astro config

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -3,12 +3,18 @@ import react from '@astrojs/react';
 import vercelServerless from '@astrojs/vercel/serverless';
 import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import { config } from '../common/config';
 import { plugins as postCssPlugins } from './postcss.config.mjs';
 
-const visualEditingEnabled =
-  import.meta.env.SANITY_VISUAL_EDITING_ENABLED === 'true';
+const { SANITY_VISUAL_EDITING_ENABLED } = loadEnv(
+  process.env.NODE_ENV || 'development',
+  process.cwd(),
+  ''
+);
+
+const visualEditingEnabled = SANITY_VISUAL_EDITING_ENABLED === 'true';
 
 export default defineConfig({
   output: visualEditingEnabled ? 'server' : 'static',
