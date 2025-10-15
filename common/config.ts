36989d90803de84d282b06b6753b38682c966870
Mike Farrow <mike@farrow.io>
2025-10-15 10:23:15 +0100

    updating app url

--- common/config.ts
+++ common/config.ts
@@ -12,7 +12,7 @@ export const config = ({ dataset }: { dataset?: string }) => ({
   },
   url: {
     app: production
-      ? 'https://mike-farrow-portfolio-sanity-app.vercel.app'
+      ? 'https://mike-farrow-portfolio-sanity-next.vercel.app'
       : 'http://127.0.0.1:3000',
     studio: production
       ? 'https://mike-farrow-portfolio-sanity-studio.vercel.app'
