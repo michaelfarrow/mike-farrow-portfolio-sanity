b84ce1cc58dd42790042be35692d42615576f49e
Mike Farrow <mike@farrow.io>
2025-10-16 11:03:05 +0100

    removing comlink dep to save bundle size

--- app/src/hooks/sanity.ts
+++ app/src/hooks/sanity.ts
@@ -1,11 +1,19 @@
-import { isMaybePresentation as sanityIsMaybePresentation } from '@sanity/presentation-comlink';
-
 import { useEffect, useState } from 'react';
 
+export function isMaybePreviewIframe(): boolean {
+  return window.self !== window.top;
+}
+export function isMaybePreviewWindow(): boolean {
+  return Boolean(window.opener);
+}
+export function isMaybePresentation(): boolean {
+  return isMaybePreviewIframe() || isMaybePreviewWindow();
+}
+
 export function useIsMaybePresentation() {
-  const [current, setCurrent] = useState(false);
+  const [current, setCurrent] = useState(true);
   useEffect(() => {
-    setCurrent(sanityIsMaybePresentation());
+    setCurrent(isMaybePresentation());
   }, []);
   return current;
 }
